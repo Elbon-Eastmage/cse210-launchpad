c8478e19073013236c1b75576d31595eb7a21372
Elbon Eastmage <elbon.eastmage@gmail.com>
2026-05-05 15:40:01 -0600

    Completed C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+        
+        do
+        {
+            Random random = new();
+            int magicNumber = random.Next(1, 101);
+            int attempts = 0;
+            int guess;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+                attempts++;
+
+            } while (guess != magicNumber);
+
+            if (attempts == 1)
+            {
+                Console.WriteLine("You made 1 guess.");
+            }
+            else
+            {
+                Console.WriteLine($"You made {attempts} guesses.");
+            }
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain == "yes");
     }
 }
