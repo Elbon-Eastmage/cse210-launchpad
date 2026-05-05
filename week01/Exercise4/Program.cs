12b31366a2f22687c7a3dd62f35a5f5e322b775c
Elbon Eastmage <elbon.eastmage@gmail.com>
2026-05-05 16:14:48 -0600

    Completed C# Programming Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int input;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        bool getInput = true;
+
+        while (getInput)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+                numbers.Add(input);
+            else
+                getInput = false;
+        }
+
+        int total = 0;
+        int largestNumber = 0;
+        int smallestPositiveNumber = int.MaxValue;
+
+        foreach (int number in numbers)
+        {
+            total += number;
+
+            if (number > largestNumber)
+                largestNumber = number;
+            
+            if (number > 0 && smallestPositiveNumber > number)
+                smallestPositiveNumber = number;
+        }
+        
+        float average = (float)total / numbers.Count;
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        Console.WriteLine("The sorted list is: ");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
