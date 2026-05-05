28e13753c826f34dd5b969990bd1434da63fdd0c
Elbon Eastmage <elbon.eastmage@gmail.com>
2026-05-05 15:06:46 -0600

    Added braces to if statements.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -10,38 +10,62 @@ class Program
         char gradeSymbol;
 
         if (gradePercentage >= 90)
+        {
             letterGrade = 'A';
+        }
         else if (gradePercentage >= 80)
+        {
             letterGrade = 'B';
+        }
         else if (gradePercentage >= 70)
+        {
             letterGrade = 'C';
+        }
         else if (gradePercentage >= 60)
+        {
             letterGrade = 'D';
+        }
         else
+        {
             letterGrade = 'F';
+        }
         
         int lastDigit = gradePercentage % 10;
 
         if (lastDigit >= 7)
+        {
             gradeSymbol = '+';
+        }
         else if (lastDigit < 3)
+        {
             gradeSymbol = '-';
+        }
         else
+        {
             gradeSymbol = ' ';
+        }
         
         if (letterGrade == 'A')
         {
             if (gradePercentage >= 97)
+            {
                 gradeSymbol = ' ';
+            }
         }
         else if (letterGrade == 'F' && gradeSymbol != ' ')
+        {
             gradeSymbol = ' ';
+        }
         
         Console.WriteLine($"Grade: {letterGrade}{gradeSymbol}");
         
         if (gradePercentage >= 70)
+        {
             Console.WriteLine("Well done! You passed the class!");
+        }
         else
+        {
             Console.WriteLine("You didn't pass. However, it's not the end. You can try again!");
+        }
     }
 }
